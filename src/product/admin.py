b047f4278ddb705fe70a46f292992edf4dadd68c
shiftgithub <mhturanmohammad@gmail.com>
2024-01-31 11:11:58 +0600

    update : admin.py

--- src/product/admin.py
+++ src/product/admin.py
@@ -1,10 +1,18 @@
 from .models import *
 from django.contrib import admin
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title','sku', 'description']
+class VariantAdmin(admin.ModelAdmin):
+    list_display = ['title','description', 'active']
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ['product','file_path']
+class ProductVariantAdmin(admin.ModelAdmin):
+    list_display = ['variant_title','variant','product']
+class ProductVariantPriceAdmin(admin.ModelAdmin):
+    list_display = ['product_variant_one','product_variant_two','product_variant_three','price','stock','product']
 
-
-
-admin.site.register(Product)
-admin.site.register(ProductImage)
-admin.site.register(ProductVariant)
-admin.site.register(ProductVariantPrice)
-admin.site.register(Variant)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Variant,VariantAdmin)
+admin.site.register(ProductImage,ProductImageAdmin)
+admin.site.register(ProductVariant,ProductVariantAdmin)
+admin.site.register(ProductVariantPrice,ProductVariantPriceAdmin)
