108de25784c432398848edb88f36c0aed9e45743
shiftgithub <mhturanmohammad@gmail.com>
2024-01-31 21:33:17 +0600

    update : working on product list

--- src/product/admin.py
+++ src/product/admin.py
@@ -15,17 +15,48 @@ class ProductImageAdmin(admin.ModelAdmin):
 
 
 class ProductVariantAdmin(admin.ModelAdmin):
-    list_display = ['variant_title', 'variant', 'product']
+    list_display = ['variant_title', 'get_variant_title', 'get_product_title']
 
+    def get_variant_title(self, obj):
+        return obj.variant.title if obj.variant else ''
 
-class ProductVariantPriceAdmin(admin.ModelAdmin):
-    list_display = ['product_variant_one', 'product_variant_two', 'product_variant_three', 'price', 'stock',
-                    'get_product_title']
+    get_variant_title.short_description = 'Variant Title'
 
     def get_product_title(self, obj):
         return obj.product.title if obj.product else ''
 
     get_product_title.short_description = 'Product Title'
+
+
+class ProductVariantPriceAdmin(admin.ModelAdmin):
+    list_display = ['get_product_variant_one', 'get_product_variant_two', 'get_product_variant_three',
+                    'price', 'stock', 'get_product_title']
+
+    def get_product_title(self, obj):
+        return obj.product.title if obj.product else ''
+
+    get_product_title.short_description = 'Product Title'
+
+    def get_product_variant_one(self, obj):
+        if obj.product_variant_one:
+            return obj.product_variant_one.variant_title
+        return ''
+
+    get_product_variant_one.short_description = 'Product Variant One'
+
+    def get_product_variant_two(self, obj):
+        if obj.product_variant_two:
+            return obj.product_variant_two.variant_title
+        return ''
+
+    get_product_variant_two.short_description = 'Product Variant Two'
+
+    def get_product_variant_three(self, obj):
+        if obj.product_variant_three:
+            return obj.product_variant_three.variant_title
+        return ''
+
+    get_product_variant_three.short_description = 'Product Variant Three'
 
 
 admin.site.register(Product, ProductAdmin)
