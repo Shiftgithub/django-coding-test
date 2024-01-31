7e5aa332ed0296949eaa7b5318459c957ab815dc
shiftgithub <mhturanmohammad@gmail.com>
2024-01-31 18:50:08 +0600

    update : add product

--- src/product/admin.py
+++ src/product/admin.py
@@ -1,18 +1,35 @@
 from .models import *
 from django.contrib import admin
+
+
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title','sku', 'description']
+    list_display = ['title', 'sku', 'description']
+
+
 class VariantAdmin(admin.ModelAdmin):
-    list_display = ['title','description', 'active']
+    list_display = ['title', 'description', 'active']
+
+
 class ProductImageAdmin(admin.ModelAdmin):
-    list_display = ['product','file_path']
+    list_display = ['product', 'file_path']
+
+
 class ProductVariantAdmin(admin.ModelAdmin):
-    list_display = ['variant_title','variant','product']
+    list_display = ['variant_title', 'variant', 'product']
+
+
 class ProductVariantPriceAdmin(admin.ModelAdmin):
-    list_display = ['product_variant_one','product_variant_two','product_variant_three','price','stock','product']
+    list_display = ['product_variant_one', 'product_variant_two', 'product_variant_three', 'price', 'stock',
+                    'get_product_title']
+
+    def get_product_title(self, obj):
+        return obj.product.title if obj.product else ''
+
+    get_product_title.short_description = 'Product Title'
+
 
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Variant,VariantAdmin)
-admin.site.register(ProductImage,ProductImageAdmin)
-admin.site.register(ProductVariant,ProductVariantAdmin)
-admin.site.register(ProductVariantPrice,ProductVariantPriceAdmin)
+admin.site.register(Variant, VariantAdmin)
+admin.site.register(ProductImage, ProductImageAdmin)
+admin.site.register(ProductVariant, ProductVariantAdmin)
+admin.site.register(ProductVariantPrice, ProductVariantPriceAdmin)
--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,14 +1,35 @@
 from django.views import generic
+from django.views.generic import ListView, CreateView, UpdateView
 
-from product.models import Variant
+from product.models import *
+from product.forms import *
 
 
-class CreateProductView(generic.TemplateView):
+class BaseProductView(generic.View):
+    form_class = ProductForm
+    model = Product
     template_name = 'products/create.html'
+    success_url = '/product/create.product'
 
-    def get_context_data(self, **kwargs):
-        context = super(CreateProductView, self).get_context_data(**kwargs)
-        variants = Variant.objects.filter(active=True).values('id', 'title')
-        context['product'] = True
-        context['variants'] = list(variants.all())
-        return context
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/list.html'
+    context_object_name = 'products'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        # Apply filtering logic based on request parameters
+        title_filter = self.request.GET.get('title')
+        if title_filter:
+            queryset = queryset.filter(title__icontains=title_filter)
+        # Add more filters as needed
+        return queryset
+
+class ProductCreateView(BaseProductView,CreateView):
+    success_url = '/product/create/'
+
+
+class ProductEditView(BaseProductView, UpdateView):
+    pk_url_kwarg = 'id'
